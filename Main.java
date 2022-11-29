c3c2cee83fd254d532b5487a9fe83ee3b9bd42d3
Larry Le <lle114@student.hbuhsd.edu>
2022-11-28 22:50:20 -0800

    Push 3

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main{
     public static void main(String[] args){
         
-        for (int i = 0; i < 200; i += 2){
-            System.out.println(i);
+        for (int j = 0; j < 200; j += 2){
+            System.out.println(j);
         }
 
     }
